a48605dcd772d12f0e34020554b7304ddfe0aed5
github username <github email address>
2021-05-24 19:34:13 -0500

    asdf added create action in tests controller

--- app/controllers/api/v1/tests_controller.rb
+++ app/controllers/api/v1/tests_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::TestsController < ApplicationController
+
+    def show 
+        test = Test.find_by(test_params)
+        render json: {test: test}, status: :accepted
+    end
+
+    def create
+        test = Test.create(test_params)
+        render json: {test: test}, status: :created
+    end
+
+    private
+
+    def test_params
+        params.require(:test).permit(:user_id, :results, :ive, :svn, :tvf, :pvj)
+    end
 end
--- db/migrate/20210523174956_create_tests.rb
+++ db/migrate/20210523174956_create_tests.rb
@@ -2,7 +2,7 @@ class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
       t.integer :user_id
-      t.string :results
+      t.string :results, default: nil
       t.integer :ive, default: 45
       t.integer :svn, default: 45
       t.integer :tvf, default: 45
